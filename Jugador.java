1d84566c6e1cd34198a6aa821662545518317dd2
chrispincas <christian.pinzon92@gmail.com>
2021-09-14 21:39:44 -0500

    Primer commit

---
+++ Jugador.java
@@ -0,0 +1,19 @@
+public class Jugador {
+    //              ^ (Jugador hereda de Personaje)
+    //Inserte acá los atributos
+    
+    
+    
+    //Inserte acá el método constructor
+    
+    
+    
+    //Inserte acá los métodos (NO LOS GETTER Y SETTERS)
+    
+    
+    
+    //Inserte acá los SETTERS Y GETTERS
+    
+    
+    
+}
